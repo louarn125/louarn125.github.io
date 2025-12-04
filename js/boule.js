225c0dc1528f9cb9e83d6f1c512525f9da69725b
louarn152 <louarn125@gmail.com>
2025-12-04 23:00:46 +0100

    update JS
    update boule
     color
    ajout cadre

--- js/boule.js
+++ js/boule.js
@@ -3,16 +3,17 @@ console.log("roux")
 class boule {
     /**
      * 
-     * @param {*} x 
-     * @param {*} y 
-     * @param {*} rayon 
+     * @param {*} x coord axe x centre de la boule
+     * @param {*} y coord axe y centre de la boule
+     * @param {*} rayon rayon de la boule
      */
-    constructor(x,y,rayon){
+    constructor(x,y,rayon,color){
         this.rayon = rayon;
         this.x = x;
         this.y = y;
         this.v_x = 0;
         this.v_y = 0;
+        this.color = color;
     }
     move(){
         this.x+=this.v_x;
@@ -20,5 +21,14 @@ class boule {
     }
     gravite(){
         this.v_y += 5;
+    }
+}
+
+class cadre {
+    constructor(width,height){
+        this.x = 0;
+        this.y = 0;
+        this.width = width;
+        this.height = height;
     }
 }
