cf18e84a7fe7ec698b1bb666744b8a247904948a
louarn152 <louarn125@gmail.com>
2025-12-04 22:46:01 +0100

    class boule
    BOULE

--- js/boule.js
+++ js/boule.js
@@ -1,1 +1,24 @@
 console.log("roux")
+
+class boule {
+    /**
+     * 
+     * @param {*} x 
+     * @param {*} y 
+     * @param {*} rayon 
+     */
+    constructor(x,y,rayon){
+        this.rayon = rayon;
+        this.x = x;
+        this.y = y;
+        this.v_x = 0;
+        this.v_y = 0;
+    }
+    move(){
+        this.x+=this.v_x;
+        this.y+=this.v_y;
+    }
+    gravite(){
+        this.v_y += 5;
+    }
+}
