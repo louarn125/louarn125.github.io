0ae04becabb139f9a46f53e2d19eb4f1009e7a40
Hoken <lol.kinx14.10.2007@gmail.com>
2025-12-05 05:20:12 +0100

    sa grand mere le nain

--- questionnaire.js
+++ questionnaire.js
@@ -19,12 +19,14 @@ function flip_flop_name(name) {
 }
 
 function append_childs(name_of_parent, elements_name) {
-  //console.log("appenning");
+  // console.log("appenning");
+  // console.log(name_of_parent, ":", document.getElementById(name_of_parent));
   for (
     let idx_xml_question = 0;
     idx_xml_question < xml_doc.getElementsByTagName(elements_name).length;
     idx_xml_question++
   ) {
+    console.log(xml_doc.getElementsByTagName(elements_name)[idx_xml_question]);
     document.getElementById(name_of_parent).innerHTML =
       document.getElementById(name_of_parent).innerHTML +
       "<option id=" +
@@ -37,14 +39,14 @@ function append_childs(name_of_parent, elements_name) {
   }
 }
 
-function load_xml_file() {
+function load_xml_file(page) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       // j'ai un truc
       ready_htpp_request = true;
       xml_doc = xmlhttp.responseXML.getElementsByTagName("data")[0];
-      init();
+      init(page);
 
       //create_exercice(1000, ["haut"], 1);
     }
@@ -52,9 +54,13 @@ function load_xml_file() {
   xmlhttp.open("GET", "https://louarn125.github.io/data.xml", true);
   xmlhttp.send();
 }
-function init() {
+function init(page) {
   dynamic_charge_all();
-  append_childs("select_q_sport_pratique", "sport"); //et similaire si autre bdd pour réponse aux qcm
+  if (page == "questionnaire.html") {
+    append_childs("select_q_sport_pratique", "sport"); //et similaire si autre bdd pour réponse aux qcm
+  } else if (page == "exercices.html") {
+    append_childs("select_groupe_muscu", "muscle");
+  }
 }
 
 function dynamic_change(name) {
@@ -78,7 +84,6 @@ function dynamic_charge_all() {
       if (better_stored_value.startsWith("box_")) {
         document.getElementById(better_stored_value).checked =
           localStorage.getItem(stored_value) === "true";
-        //console.log("je flip flop", better_stored_value.slice(4));
         flip_flop_name(better_stored_value.slice(4));
       } else if (better_stored_value.startsWith("text_")) {
         document.getElementById(better_stored_value).value =
@@ -88,85 +93,95 @@ function dynamic_charge_all() {
   }
 }
 
-function load_page() {
-  load_xml_file();
+function load_page(page) {
+  load_xml_file(page);
   document.cookie += ";"; // pour que la fonction marche bien tqt
 }
 
-function encookization() {
+function encookization(page) {
   //document.cookie = newCookie;
+  if (page == "questionnaire.html") {
+    // fequence current
+    setCookie(
+      "frequence_current",
+      document.getElementById("select_frequence_current").value,
+      10,
+    );
 
-  // fequence current
-  setCookie(
-    "frequence_current",
-    document.getElementById("select_frequence_current").value,
-    10,
-  );
+    // frequence promise
+    setCookie(
+      "frequence_promise",
+      document.getElementById("select_frequence_promise").value,
+      10,
+    );
 
-  // frequence promise
-  setCookie(
-    "frequence_promise",
-    document.getElementById("select_frequence_promise").value,
-    10,
-  );
+    // sport_pratique
+    setCookie(
+      "sport_pratique",
+      document.getElementById("box_sport_pratique").checked,
+      10,
+    );
 
-  // sport_pratique
-  setCookie(
-    "sport_pratique",
-    document.getElementById("box_sport_pratique").checked,
-    10,
-  );
+    // q_sport_pratique
+    setCookie(
+      "q_sport_pratique",
+      document.getElementById("q_sport_pratique").value,
+      10,
+    );
 
-  // q_sport_pratique
-  setCookie(
-    "q_sport_pratique",
-    document.getElementById("q_sport_pratique").value,
-    10,
-  );
+    // raison_sport
+    setCookie(
+      "raison_sport",
+      document.getElementById("select_raison_sport").value,
+      10,
+    );
 
-  // raison_sport
-  setCookie(
-    "raison_sport",
-    document.getElementById("select_raison_sport").value,
-    10,
-  );
+    // competition
+    setCookie(
+      "competition", //document.cookie = document.cookie + cname + "=" + cvalue + ";";
+      //document.cookie = "roux=2;";
+      //document.cookie = "caca=1;";
+      document.getElementById("box_competition").checked,
+      10,
+    );
 
-  // competition
-  setCookie(
-    "competition", //document.cookie = document.cookie + cname + "=" + cvalue + ";";
-    //document.cookie = "roux=2;";
-    //document.cookie = "caca=1;";
-    document.getElementById("box_competition").checked,
-    10,
-  );
+    // cb_competition
+    setCookie(
+      "cb_competition",
+      document.getElementById("text_competition").textContent,
+      10,
+    );
 
-  // cb_competition
-  setCookie(
-    "cb_competition",
-    document.getElementById("text_competition").textContent,
-    10,
-  );
+    // nv_competition
+    setCookie(
+      "nv_competition",
+      document.getElementById("select_competition").value,
+      10,
+    );
 
-  // nv_competition
-  setCookie(
-    "nv_competition",
-    document.getElementById("select_competition").value,
-    10,
-  );
+    // next_comp
+    setCookie(
+      "next_competition",
+      document.getElementById("box_next_competition").checked,
+      10,
+    );
 
-  // next_comp
-  setCookie(
-    "next_competition",
-    document.getElementById("box_next_competition").checked,
-    10,
-  );
+    // q_next_comp
+    setCookie(
+      "nv_next_comb",
+      document.getElementById("q_next_competition").value,
+      10,
+    );
+  } else if (page == "exercices") {
+    setCookie(
+      "muscles",
+      document.getElementById("select_groupe_muscu").value,
+      10,
+    );
 
-  // q_next_comp
-  setCookie(
-    "nv_next_comb",
-    document.getElementById("q_next_competition").value,
-    10,
-  );
+    setCookie("equipement", document.getElementById("temp_dispo").value, 10);
+  }
+
   //alert(document.cookie);
 }
 
